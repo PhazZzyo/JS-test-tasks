4bb7e949eb7a0756f6ecb3323f9afe91cb3a1bbe
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-06 12:08:48 +0200

    save

---
+++ js/scrips.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// -------------- --------- ---- ====***** ( Exercise ) #25
+
+function getCommonElements(array1, array2) {
+  // Change code below this line
+  
+  const array3 = [];
+
+for (const element of array1) {
+    if (array2.includes(element)) {     
+      array3.push(element);
+  }
+  }
+  return array3;
+ // Change code above this line
+}
